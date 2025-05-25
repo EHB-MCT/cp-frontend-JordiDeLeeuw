a584956285fc33fa205f2dbc1b2ffb512d462ced
JordiDeLeeuw <jordi.de.leeuw@student.ehb.be>
2025-05-25 22:19:11 +0200

    added another comment

--- src/components/portal/Making.tsx
+++ src/components/portal/Making.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 const base = import.meta.env.BASE_URL;
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 import "../../styles/portal/MakingOf.scss";
 
 type Story = {
